b14dc7747d0b5a6d2680e7d50baf163df3f82b8e
maisui <aibofan95@gmail.com>
2025-03-27 15:31:37 +0800

    将get_docker_ips的输出合并在一起

--- get_docker_ips.sh
+++ get_docker_ips.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-echo "获取 Docker 运行的容器 IP 地址："
+echo "获取所有 Docker 运行的容器 IP 地址："
+echo "-------------------------------------"
+
+# 获取所有容器的 IP
 docker ps -q | while read -r container_id; do
     container_name=$(docker inspect --format '{{.Name}}' "$container_id" | sed 's/^\///')
     container_ip=$(docker inspect --format '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$container_id")
-    echo "$container_name: $container_ip"
+    if [[ -n "$container_ip" ]]; then
+        printf "%-30s %s\n" "$container_name" "$container_ip"
+    fi
 done
 
-echo -e "\n获取 Docker Compose 运行的容器 IP 地址："
+# 获取 Docker Compose 相关网络下的所有容器 IP
 docker network ls --format '{{.Name}}' | while read -r network; do
-    docker network inspect "$network" --format '{{range .Containers}}{{.Name}}: {{.IPv4Address}}{{"\n"}}{{end}}'
+    docker network inspect "$network" --format '{{range .Containers}}{{.Name}} {{.IPv4Address}}{{"\n"}}{{end}}'
+done | while read -r container_name container_ip; do
+    if [[ -n "$container_ip" ]]; then
+        printf "%-30s %s\n" "$container_name" "${container_ip%/*}"  # 移除 CIDR 格式
+    fi
 done
+
+echo "-------------------------------------"
